794d858825b20b7e32577db5425e7a8fcff8523c
Mehedi Hossen Fahim <mehdihossenfahim100@gmail.com>
2026-05-13 02:00:00 +0600

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function rollDice() {
 
     // Show winner
     if (randomNumber1 > randomNumber2) {
-        document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+        document.querySelector("h1").textContent = "Player 1 Wins!";
     } else if (randomNumber1 < randomNumber2) {
-        document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+        document.querySelector("h1").textContent = "Player 2 Wins!";
     } else {
-        document.querySelector("h1").textContent = "🏳️ Draw! 🏳️";
+        document.querySelector("h1").textContent = "Draw!";
     }
 }
 
