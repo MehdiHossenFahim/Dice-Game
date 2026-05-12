65f88b68f87767e6aaa626324a714e12ccd0451d
Mehedi Hossen Fahim <mehdihossenfahim100@gmail.com>
2026-05-12 23:01:45 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomImage1 ="./images/dice"+randomNumber1+".png"
+document.querySelector(".img1").setAttribute("src",randomImage1);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomImage2 ="./images/dice"+randomNumber2+".png"
+document.querySelector(".img2").setAttribute("src",randomImage2);
+
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+}else{
+    document.querySelector("h1").textContent = "🏳️Draw!🏳️";
+}
+document.querySelector("button").classList.remove("invisible");
+document.querySelector("button").addEventListener("click",handleClick);
+
+function handleClick(){
+    location.reload();
+}
+
